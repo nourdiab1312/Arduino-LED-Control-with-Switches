0343435ca70d4c50c93df1dad903d97b8f271032
nourdiab1312 <138535140+nourdiab1312@users.noreply.github.com>
2023-07-28 12:40:59 +0300

    Update sketch_jul16a.ino

--- sketch_jul16a.ino
+++ sketch_jul16a.ino
@@ -1,53 +1,67 @@
-int Reading=0;
-void setup()
+int SS1=2; 
+int SS2=3; 
+
+int R=0;
+
+int I=0;
+int L1=5;
+int L2=6;
+int L3=7;
+int L4=8;
+int L5=9;
+int L6=10;
+  void setup()
 {
-  pinMode(A0,INPUT);
-  pinMode(A1,INPUT);
-  pinMode(12,OUTPUT);
-  pinMode(11,OUTPUT);
-  pinMode(10,OUTPUT);
-  pinMode(9,OUTPUT);
-  pinMode(8,OUTPUT);
-  pinMode(7,OUTPUT);
+  
+  pinMode(L1, OUTPUT);
+  pinMode(L2, OUTPUT);
+  pinMode(L3, OUTPUT);
+  pinMode(L4, OUTPUT);
+  pinMode(L5, OUTPUT);
+  pinMode(L6, OUTPUT);
+  pinMode(SS1, INPUT);
+  pinMode(SS2, INPUT);
+    
 }
 
 void loop()
 {
- Reading=analogRead(A0);
-  analogWrite(12,Reading/4);
-   Reading=analogRead(A1);
-  analogWrite(7,Reading/4);
-  
- /* digitalWrite(12,HIGH);
-  delay(Reading);
-  digitalWrite(12,LOW);
-  delay(Reading);*/
-  
-   digitalWrite(11,HIGH);
-  delay(Reading);
-  digitalWrite(11,LOW);
-  delay(Reading);
-  
-   digitalWrite(10,HIGH);
-  delay(Reading);
-  digitalWrite(10,LOW);
-  delay(Reading);
-  
-   digitalWrite(9,HIGH);
-  delay(Reading);
-  digitalWrite(9,LOW);
-  delay(Reading);
-  
-   digitalWrite(8,HIGH);
-  delay(Reading);
-  digitalWrite(8,LOW);
-  delay(Reading);
-  
-  /* digitalWrite(7,HIGH);
-  delay(Reading);
-  digitalWrite(7,LOW);
-  delay(Reading);*/
-  
+  R=digitalRead(SS1);
+   if (R==HIGH)
+  {
    
+  digitalWrite(8, HIGH);
+  for (I=0;I<=128;I++)
+  {analogWrite(5,HIGH);}
+   
+  }
+   
+  else
+  {
+    digitalWrite(8, LOW);
+    digitalWrite(9, LOW);
+  for (I=128;I>=0;I++)
+  {analogWrite(5,LOW);}
+   while (SS1==LOW);
+   
+  }
+  R=digitalRead(SS2);
+   if (R==HIGH)
+   {
+  digitalWrite(7, HIGH);
+  for (I=0;I<=128;I++)
+  {analogWrite(6,HIGH);}
   
+  }
+   
+  else
+  {
+    digitalWrite(7, LOW);
+    digitalWrite(10, LOW);
+  for (I=128;I>=0;I++)
+  {analogWrite(6,LOW);}
+   while (SS2==LOW);
+  
+  }
 }
+ 
